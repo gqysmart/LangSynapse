5031db8acd0ddf25898be9974c4a9bcb7d930077
qiyunge <157900653@qq.com>
2025-05-25 15:41:14 -0400

    for vercel auto deploy

--- src/app/actions/note.action.ts
+++ src/app/actions/note.action.ts
@@ -1,14 +1,14 @@
 // app/actions.ts
 "use server"
-import getModelWithOpenAI from "@/lib/langchain/ai/openAI"
-import {HumanMessage} from "@langchain/core/messages"
-import {SystemMessage} from "@langchain/core/messages"
-import {
-  MessageContent,
-  // MessageContentComplex,
-} from "@langchain/core/messages"
+// import getModelWithOpenAI from "@/lib/langchain/ai/openAI"
+// import {HumanMessage} from "@langchain/core/messages"
+// import {SystemMessage} from "@langchain/core/messages"
+// import {
+//   MessageContent,
+//   // MessageContentComplex,
+// } from "@langchain/core/messages"
 
-import {PrismaClient} from "@/generated/prisma"
+import {PrismaClient} from "@prisma/client"
 
 export async function voiceAction(formData: FormData) {
   const text = formData.get("text")
@@ -43,45 +43,45 @@ export async function handleNoteAction(
   await createNote(noteContent)
 
   //根据用户不同给出不同的systemMessage
-  const systemMsg = new SystemMessage(
-    `Tu es un professeur de grammaire française.
+  //   const systemMsg = new SystemMessage(
+  //     `Tu es un professeur de grammaire française.
 
-      Quand un étudiant te donne une phrase, analyse-la.
+  //       Quand un étudiant te donne une phrase, analyse-la.
 
-      1. Vérifie s’il y a une ou plusieurs erreurs de grammaire, d’accord, de conjugaison, de genre, d’article, ou d’usage.
+  //       1. Vérifie s’il y a une ou plusieurs erreurs de grammaire, d’accord, de conjugaison, de genre, d’article, ou d’usage.
 
-      2 . S’il y a des erreurs, corrige la phrase.
+  //       2 . S’il y a des erreurs, corrige la phrase.
 
-      3. Pour chaque erreur, indique :
-         - Où se trouve l’erreur (location),
-         - Quelle est la correction exacte (correction),
-         - Et pourquoi cette modification est nécessaire (raison), en français simple.
+  //       3. Pour chaque erreur, indique :
+  //          - Où se trouve l’erreur (location),
+  //          - Quelle est la correction exacte (correction),
+  //          - Et pourquoi cette modification est nécessaire (raison), en français simple.
 
-      4. Réponds uniquement en JSON, dans le format suivant :
+  //       4. Réponds uniquement en JSON, dans le format suivant :
 
-   {
-  
-  "errors": [
-    {
-      "location": "...",
-      "correction": "...",
-      "reason": "..."
-    }
-  ]
-}
-`
-  )
+  //    {
+
+  //   "errors": [
+  //     {
+  //       "location": "...",
+  //       "correction": "...",
+  //       "reason": "..."
+  //     }
+  //   ]
+  // }
+  // `
+  //   )
 
   // Do something, like validation or API call...
 
-  function extractText(content: MessageContent): string {
-    if (typeof content === "string") return content
+  // function extractText(content: MessageContent): string {
+  //   if (typeof content === "string") return content
 
-    return content
-      .filter((c): c is {type: "text"; text: string} => c.type === "text")
-      .map((c) => c.text)
-      .join("\n")
-  }
+  //   return content
+  //     .filter((c): c is {type: "text"; text: string} => c.type === "text")
+  //     .map((c) => c.text)
+  //     .join("\n")
+  // }
 
   // const openAIModel = await getModelWithOpenAI()
   // const res = await openAIModel.invoke([
